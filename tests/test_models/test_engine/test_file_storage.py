2906efd22a21b4f36a720499026d95061e102ce5
mistWil <7448@holbertonstudents.com>
2024-03-04 20:34:17 +0100

    test

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+
+
+import unittest
+import os
+from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
+
+
+class TestFileStorage(unittest.TestCase):
+    def setUp(self):
+        self.storage = FileStorage()
+
+    def tearDown(self):
+        try:
+            os.remove("file.json")
+        except FileNotFoundError:
+            pass
+
+    def test_all(self):
+        pass
+        # all_objs = self.storage.all()
+        # self.assertEqual(all_objs, {})
+
+    def test_new(self):
+        pass
+        # obj = BaseModel()
+        # self.storage.new(obj)
+        # all_objs = self.storage.all()
+        # self.assertEqual(len(all_objs), 1)
+
+    def test_save_and_reload(self):
+        obj = BaseModel()
+        self.storage.new(obj)
+        self.storage.save()
+        new_storage = FileStorage()
+        new_storage.reload()
+        all_objs = new_storage.all()
+        self.assertEqual(len(all_objs), 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
