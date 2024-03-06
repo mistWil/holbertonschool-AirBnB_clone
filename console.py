a8219f85aa281073b8402fbeda16237fb61bf7d8
mistWil <7448@holbertonstudents.com>
2024-03-06 10:08:11 +0100

    add help methode

--- console.py
+++ console.py
@@ -21,5 +21,18 @@ class HBNBCommand(cmd.Cmd):
         """Do nothing when an empty line is entered"""
         pass
 
+    def do_help(self, arg):
+        """do_help function displays documantation"""
+        words = "Documented commands (type help <topic>):"
+        if arg:
+            super().do_help(arg)
+        else:
+            print("\n{}".format(words), end='\n')
+            print('=' * len(words), end='\n')
+            for command in self.get_names():
+                if command.startswith("do"):
+                    print("{}".format(command[3:]), end=" ")
+            print("\n")
+
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
