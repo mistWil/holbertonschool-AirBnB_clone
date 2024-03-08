a585d446c3ea6f8ebbe85481adac3dd5cb53f50b
mistWil <7448@holbertonstudents.com>
2024-03-08 10:20:55 +0100

    adds

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -42,14 +42,24 @@ class FileStorage:
                 obj_dict = json.load(file)
                 from models.base_model import BaseModel
                 from models.user import User
+                from models.city import City
+                from models.place import Place
+                from models.review import Review
+                from models.state import State
                 for key, value in obj_dict.items():
                     class_name, obj_id = key.split('.')
-                    if class_name == 'BaseModel':
-                        obj = BaseModel(**value)
-                    elif class_name == 'User':
+                    if class_name == "User":
                         obj = User(**value)
+                    elif class_name == "City":
+                        obj = City(**value)
+                    elif class_name == "Place":
+                        obj = Place(**value)
+                    elif class_name == "Review":
+                        obj = Review(**value)
+                    elif class_name == "State":
+                        obj = State(**value)
                     else:
-                        continue
+                        obj = BaseModel(**value)
                     self.__objects[key] = obj
         except FileNotFoundError:
             pass
